daa60c0694460759c454322a40ce33ac2e36430a
subnare <shrutikaubnare@gmail.com>
2026-04-21 15:49:23 -0400

    Add ExpenseService interface

--- enterprise/java/com/expenseplanner/enterprise/service/ExpenseService.java
+++ enterprise/java/com/expenseplanner/enterprise/service/ExpenseService.java
@@ -1,4 +1,12 @@
 package java.com.expenseplanner.enterprise.service;
 
+import java.util.List;
+import java.com.expenseplanner.enterprise.model.Expense;
+
 public interface ExpenseService {
+    List<Expense> getAllExpenses();
+    Expense getExpenseById(Long id);
+    Expense addExpense(Expense expense);
+    Expense updateExpense(Expense expense);
+    void deleteExpense(Long id);
 }
